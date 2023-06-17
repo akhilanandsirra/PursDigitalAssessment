42880743a6a029b7bcb9f0bc656385b4b330eff7
akhilanandsirra <akhilanand9999@gmail.com>
2023-06-16 23:47:51 -0400

    Initial Commit

---
+++ PursDigitalAssessment/PursDigitalAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PursDigitalAssessmentApp.swift
+//  PursDigitalAssessment
+//
+//  Created by Akhil Anand Sirra on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PursDigitalAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
